e47a1652467a544ab218d4e2c281993b975c6289
otofu-square <otofu.square@gmail.com>
2019-03-01 01:06:46 +0900

    :note: Add test for useCounter hook

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -1,9 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Counter } from "./Counter";
+import { renderHook, act, cleanup } from "react-hooks-testing-library";
+import { useCounter } from "./Counter";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Counter />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("count state", () => {
+  afterEach(cleanup);
+
+  const { result } = renderHook(() => useCounter());
+
+  // initial count value
+  expect(result.current.count).toBe(0);
+
+  // increment
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(1);
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(2);
+
+  // decrement
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(1);
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(0);
 });
